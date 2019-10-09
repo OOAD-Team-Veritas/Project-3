d29cf0f1d013ad89e222c6de4be4624dab1bd257
Riad Shash <riadshash@yahoo.com>
2019-10-08 18:40:09 -0600

    initial commit

---
+++ Project-3/src/main/java/com/ooadteamveritas/project3/Tool.java
@@ -0,0 +1,25 @@
+package com.ooadteamveritas.project3;
+
+//Product interface (factory pattern)
+public abstract class Tool {
+    protected double price;
+    protected String name;
+    protected String category;
+    
+    public double getPrice(){
+        return this.price;
+    }
+    
+    public String getName(){
+        return this.name;
+    }
+    
+    public String getCategory(){
+        return this.category;
+    }
+    
+    public String toString() {
+        StringBuffer display = new StringBuffer();
+        return display.toString();
+    }
+}
