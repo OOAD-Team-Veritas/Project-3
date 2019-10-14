b991cf4d6bb4ad7edcb7b36dcf88d7062fd52d99
elizrobinson <eliz.robinson8@gmail.com>
2019-10-13 21:52:04 -0600

    Removed description

--- Project-3/src/main/java/com/ooadteamveritas/project3/Tool.java
+++ Project-3/src/main/java/com/ooadteamveritas/project3/Tool.java
@@ -5,7 +5,6 @@ public abstract class Tool {
     protected double price;
     protected String name;
     protected String category;
-    protected String description;
     
     public double getPrice(){
         return this.price;
